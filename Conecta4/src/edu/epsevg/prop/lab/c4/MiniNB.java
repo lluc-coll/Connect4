26ea8e516a22c8cd258830ea6bd6b4f9c8aa7841
llukins <lluc.coll.ubia@gmail.com>
2024-11-15 11:22:32 +0100

    minimax amb i sense poda

--- Conecta4/src/edu/epsevg/prop/lab/c4/MiniNB.java
+++ Conecta4/src/edu/epsevg/prop/lab/c4/MiniNB.java
@@ -11,25 +11,25 @@ import static java.lang.Integer.min;
  * @author wangm
  */
 public class MiniNB implements Jugador, IAuto {
-    
+
     private String nombre;
     private int jugadasExploradas = 0;
     private int profundidad = 8;
     private boolean poda;
     private int colorNB = 1;
-    
+
     private int MAS_INFINITO = Integer.MAX_VALUE;
     private int MENOS_INFINITO = Integer.MIN_VALUE;
-    
-    private int[][] tablaPuntuacion= {{3, 4,  5,  7,  7,  5, 4, 3}, 
-                                      {4, 6,  8, 11, 11,  8, 6, 4}, 
-                                      {5, 8, 11, 13, 13, 11, 8, 5}, 
-                                      {5, 8, 11, 13, 13, 11, 8, 5}, 
-                                      {5, 8, 11, 13, 13, 11, 8, 5}, 
-                                      {5, 8, 11, 13, 13, 11, 8, 5}, 
-                                      {4, 6,  8,  7,  7,  8, 6, 4},
-                                      {3, 4,  5,  7,  7,  5, 4, 3}};
-    
+
+    private int[][] tablaPuntuacion = {{3, 4, 5, 7, 7, 5, 4, 3},
+    {4, 6, 8, 11, 11, 8, 6, 4},
+    {5, 8, 11, 13, 13, 11, 8, 5},
+    {5, 8, 11, 13, 13, 11, 8, 5},
+    {5, 8, 11, 13, 13, 11, 8, 5},
+    {5, 8, 11, 13, 13, 11, 8, 5},
+    {4, 6, 8, 7, 7, 8, 6, 4},
+    {3, 4, 5, 7, 7, 5, 4, 3}};
+
     public MiniNB(int depth, boolean Poda) {
         nombre = "MiniNB";
         jugadasExploradas = 0;
@@ -38,12 +38,16 @@ public class MiniNB implements Jugador, IAuto {
     }
 
     @Override
-    public int moviment(Tauler t, int color) {                
+    public int moviment(Tauler t, int color) {
         colorNB = color;
-        int columna_elegida = miniMax(t);
+        //int columna_elegida = miniMax(t);
         System.out.println("Numero de nodos explorados: " + jugadasExploradas);
-        
-        return columna_elegida;
+        jugadasExploradas = 0;
+        //return columna_elegida;
+        if (poda) {
+            return minimaxPoda(t, profundidad, true, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        }
+        return minimax(t, profundidad, true);
     }
 
     @Override
@@ -51,63 +55,100 @@ public class MiniNB implements Jugador, IAuto {
         return nombre;
     }
 
-    private int miniMax(Tauler t, int prof, boolean esMax) {
-        int actual = Integer.MIN_VALUE, columnaJugar = 0;
-        int alpha = Integer.MIN_VALUE, beta = Integer.MAX_VALUE;
-        
-        for (int col = 0; col < t.getMida(); ++col){
-            if (t.movpossible(col)) {
-                Tauler tablaNueva = new Tauler(t);
-                tablaNueva.afegeix(col, colorNB);
-                int min = valorMin(tablaNueva, col, profundidad -1, alpha, beta);
-                if (min > actual) {
-                    actual = min;
-                    columnaJugar = col;
-                }
+    private int minimax(Tauler t, int prof, boolean esMax) {
+        if (prof == 0) {
+            int heuristica = 0; //funcion heuristica
+            return heuristica;
         }
-        return columnaJugar;
-    }
+        if (esMax) {
+            int colMax = 0;
+            int max = Integer.MIN_VALUE; //funcion heuristica???
+            for (int i = 0; i < 8; i++) {
+                if (t.movpossible(i)) {
+                    Tauler nou = new Tauler(t);
+                    nou.afegeix(i, 1);
+                    int aux = minimax(nou, prof - 1, false);
+                    if (max < aux) {
+                        max = aux;
+                        colMax = i;
+                    }
+                    
+                    jugadasExploradas++;
+                }
 
-    private int valorMin(Tauler T, int col, int prof, int alpha, int beta) {
-       int actual = Integer.MIN_VALUE;
-       if (solucio(col, colorNB)) 
-    }
-
-    private int valorMax(Tauler T, int col, int prof, int alpha, int beta) {
+            }
+            if (prof == profundidad) {
+                return colMax;
+            }
+            return max;
             
+        } else {
+            int min = Integer.MAX_VALUE; //funcion heuristica???
+            for (int i = 0; i < 8; i++) {
+                if (t.movpossible(i)) {
+                    Tauler nou = new Tauler(t);
+                    nou.afegeix(i, -1);
+                    int aux = minimax(nou, prof - 1, true);
+                    min = Math.min(min, aux);
+                    
+                    jugadasExploradas++;
+                }
+            }
+            return min;
+        }
     }
-    
-}
 
+    private int minimaxPoda(Tauler t, int prof, boolean esMax, int alpha, int beta) {
+        if (prof == 0) {
+            int heuristica = 0; //funcion heuristica
+            return heuristica;
+        }
+        if (esMax) {
+            int colMax = 0;
+            int max = Integer.MIN_VALUE; //funcion heuristica???
+            for (int i = 0; i < 8; i++) {
+                if (t.movpossible(i)) {
+                    Tauler nou = new Tauler(t);
+                    nou.afegeix(i, 1);
+                    int aux = minimaxPoda(nou, prof - 1, false, alpha, beta);
+                    if (max < aux) {
+                        max = aux;
+                        colMax = i;
+                    }
+                    alpha = Math.max(alpha, aux);
+                    if (beta<=alpha){
+                        i = 8;
+                    }
+                    
+                    jugadasExploradas++;
+                }
+            }
+            if (prof == profundidad) {
+                return colMax;
+            }
+            return max;
+        } else {
+            int min = Integer.MAX_VALUE; //funcion heuristica???
+            for (int i = 0; i < 8; i++) {
+                if (t.movpossible(i)) {
+                    Tauler nou = new Tauler(t);
+                    nou.afegeix(i, -1);
+                    int aux = minimaxPoda(nou, prof - 1, true, alpha, beta);
+                    min = Math.min(min, aux);
+                    beta = Math.min(beta, aux);
+                    if (beta<=alpha){
+                        i = 8;
+                    }
+                    
+                    jugadasExploradas++;
+                }
+            }
+            return min;
+        }
+    }
+}
 
 // hacer minimax y heuristica separados
 // primero hacer minimax, si no tienes heuristica todavia pues hacer return 0 todo el rato
 // heuristica probar con clase prova, es decir, hacer print de la tabla
 
-
-private int minimax1(Tauler t, int prof, boolean esMax){
-    if (prof = 0){
-        int heuristica = 0; //funcion heuristica
-        return heuristica;
-    }
-    if (esMax){
-        int max = Integer.MIN_VALUE; //funcion heuristica???
-        for (int i = 0; i<8; i++){
-            Tauler nou = new Tauler(t);
-            nou.afegeix(i, 1);
-            int aux = minimax(nou, prof-1, false);
-            max = Math.max(max, aux);
-        }
-        return max;
-    }
-    else{
-        int min = Integer.MAX_VALUE; //funcion heuristica???
-        for (int i = 0; i<8; i++){
-            Tauler nou = new Tauler(t);
-            nou.afegeix(i, -1);
-            int aux = minimax(nou, prof-1, true);
-            min = Math.min(min, aux);
-        }
-        return min;
-    }
-}
